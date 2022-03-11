783080f9aeac1ef8f698a60892065bdd4213c7ee
Gustavo Menezes <91099666+guhmenezes@user.noreply.github.com>
2022-03-11 16:12:28 -0300

    update accepts digits only

--- src/br/com/guhmenezes/isthecpfvalid/Run.java
+++ src/br/com/guhmenezes/isthecpfvalid/Run.java
@@ -7,17 +7,30 @@ public class Run {
     public static void main(String[] args) {
         while (true) {
 
-            String cpf = JOptionPane.showInputDialog(null,
+            String input = JOptionPane.showInputDialog(null,
                     "Digite o CPF:", "O CPF é Válido?", JOptionPane.PLAIN_MESSAGE);
 
-            if (cpf == null)
+            if (input == null)
                 break;
 
-            String[] vector = cpf.split("");
-
+            String cpf = input.replaceAll("[^0-9]","");
+            String[] vector;
             CheckCpf requestedCpf = new CheckCpf();
-            requestedCpf.check(vector);
+
+            if (cpf.length() == 11){
+
+                vector = cpf.split("");
+                requestedCpf.check(vector);
+
+            } else {
+
+                JOptionPane.showMessageDialog(null,
+                        "Insira um CPF com 11 dígitos.", "Ops!", JOptionPane.ERROR_MESSAGE);
+
+            }
 
         }
+
     }
+
 }
